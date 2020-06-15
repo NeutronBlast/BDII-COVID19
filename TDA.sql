d6876bc04f3adc6d1eab551702dff6246dc9d582
iSlidex <45302075+iSlidex@users.noreply.github.com>
2020-06-15 19:43:18 -0400

    Update TDA.sql

--- TDA.sql
+++ TDA.sql
@@ -7,22 +7,22 @@ CREATE OR REPLACE TYPE covid_data AS OBJECT (
     STATIC FUNCTION porcentaje_fallecidos (pob NUMBER, infec NUMBER, fall NUMBER) RETURN NUMBER,
     STATIC FUNCTION porcentaje_recuperados (pob NUMBER, infec NUMBER, recup NUMBER) RETURN NUMBER
 );
-
+--Los not null en objetos se verivican con un check en el create table
 CREATE OR REPLACE TYPE persona AS OBJECT (
     img     BLOB,
-    nom1    VARCHAR(15),
+    nom1    VARCHAR(15) , --NOT NULL
     nom2    VARCHAR(15),
-    ape1    VARCHAR(15),
-    ape2    VARCHAR(15),
-    fec_nac DATE,
+    ape1    VARCHAR(15) , --NOT NULL
+    ape2    VARCHAR(15) ,  --NOT NULL
+    fec_nac DATE , --NOT NULL
     fec_mue DATE,
-    genero  VARCHAR(2),
+    genero  VARCHAR(2) ,  --NOT NULL
     STATIC FUNCTION edad (fec_nac DATE) RETURN NUMBER,
     STATIC FUNCTION size_of_img (img BLOB) RETURN NUMBER
 );
 
 CREATE OR REPLACE TYPE historia AS OBJECT (
-    fec_i DATE,
+    fec_i DATE ,  --NOT NULL
     fec_f DATE,
     STATIC FUNCTION comprobar_fechas (fec_i DATE, fec_f DATE) RETURN NUMBER
 );
