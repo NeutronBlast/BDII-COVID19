8944d42715c50442535e025f3166339229b3ce54
iSlidex <45302075+iSlidex@users.noreply.github.com>
2020-06-15 19:41:39 -0400

    Quitados not null en objetos

--- create_table.sql
+++ create_table.sql
@@ -8,22 +8,22 @@ CREATE OR REPLACE TYPE covid_data AS OBJECT (
     STATIC FUNCTION porcentaje_fallecidos (pob NUMBER, infec NUMBER, fall NUMBER) RETURN NUMBER,
     STATIC FUNCTION porcentaje_recuperados (pob NUMBER, infec NUMBER, recup NUMBER) RETURN NUMBER
 );
-
+--Los not null en objetos se verivican con un check en el create table
 CREATE OR REPLACE TYPE persona AS OBJECT (
     img     BLOB,
-    nom1    VARCHAR(15) NOT NULL,
+    nom1    VARCHAR(15) , --NOT NULL
     nom2    VARCHAR(15),
-    ape1    VARCHAR(15) NOT NULL,
-    ape2    VARCHAR(15) NOT NULL,
-    fec_nac DATE NOT NULL,
+    ape1    VARCHAR(15) , --NOT NULL
+    ape2    VARCHAR(15) ,  --NOT NULL
+    fec_nac DATE , --NOT NULL
     fec_mue DATE,
-    genero  VARCHAR(2) NOT NULL,
+    genero  VARCHAR(2) ,  --NOT NULL
     STATIC FUNCTION edad (fec_nac DATE) RETURN NUMBER,
     STATIC FUNCTION size_of_img (img BLOB) RETURN NUMBER
 );
 
 CREATE OR REPLACE TYPE historia AS OBJECT (
-    fec_i DATE NOT NULL,
+    fec_i DATE ,  --NOT NULL
     fec_f DATE,
     STATIC FUNCTION comprobar_fechas (fec_i DATE, fec_f DATE) RETURN NUMBER
 );
