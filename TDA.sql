20ebae245daf02c2c19895a9a01906fa82efcd65
iSlidex <45302075+iSlidex@users.noreply.github.com>
2020-06-19 12:30:41 -0400

    Update TDA.sql

--- TDA.sql
+++ TDA.sql
@@ -1,14 +1,77 @@
 /* TDA'S */
 CREATE OR REPLACE TYPE covid_data AS OBJECT (
     poblacion number, -- NOT NULL
-    STATIC FUNCTION numero_infectados (id_pais NUMBER) RETURN NUMBER,
-    STATIC FUNCTION numero_fallecidos (id_pais NUMBER) RETURN NUMBER,
-    STATIC FUNCTION numero_recuperados (id_pais NUMBER) RETURN NUMBER,
-    STATIC FUNCTION porcentaje_infectados (pob NUMBER, infec NUMBER) RETURN NUMBER,
-    STATIC FUNCTION porcentaje_fallecidos (pob NUMBER, infec NUMBER, fall NUMBER) RETURN NUMBER,
-    STATIC FUNCTION porcentaje_recuperados (pob NUMBER, infec NUMBER, recup NUMBER) RETURN NUMBER
+    STATIC FUNCTION numero_infectados (id_pais NUMBER) RETURN NUMBER,--done
+    STATIC FUNCTION numero_fallecidos (id_pais NUMBER) RETURN NUMBER,--done
+    STATIC FUNCTION numero_recuperados (id_pais NUMBER) RETURN NUMBER,--done
+    STATIC FUNCTION porcentaje_infectados (id_pais NUMBER) RETURN NUMBER,--done
+    STATIC FUNCTION porcentaje_fallecidos (id_pais NUMBER) RETURN NUMBER,--done
+    STATIC FUNCTION porcentaje_recuperados (id_pais NUMBER) RETURN NUMBER--done
+    -- STATIC FUNCTION porcentaje_infectados (pob NUMBER, infec NUMBER) RETURN NUMBER,
+    -- STATIC FUNCTION porcentaje_fallecidos (pob NUMBER, infec NUMBER, fall NUMBER) RETURN NUMBER,
+    -- STATIC FUNCTION porcentaje_recuperados (pob NUMBER, infec NUMBER, recup NUMBER) RETURN NUMBER
 );
 /
+--numeros infectados
+CREATE OR REPLACE TYPE BODY covid_data AS MEMBER FUNCTION numero_infectados(id_pais IN number) RETURN number
+IS resultado number;
+BEGIN
+    resultado:= SELECT COUNT(*) FROM INFECCIONES A WHERE A.id_pais=id_pais AND A.estado = "infectado";
+    return resultado; 
+END;
+--numero fallecidos
+CREATE OR REPLACE TYPE BODY covid_data AS MEMBER FUNCTION numero_fallecidos(id_pais IN number) RETURN number
+IS resultado number;
+BEGIN
+    resultado:= SELECT COUNT(*) FROM INFECCIONES A WHERE A.id_pais=id_pais AND A.estado = "muerto";
+    return resultado; 
+END;
+--numero recuperados
+CREATE OR REPLACE TYPE BODY covid_data AS MEMBER FUNCTION numero_recuperados(id_pais IN number) RETURN number
+IS resultado number;
+BEGIN
+    resultado:= SELECT COUNT(*) FROM INFECCIONES A WHERE A.id_pais=id_pais AND A.estado = "curado";
+    return resultado; 
+END;
+
+--porcentaje_infectados
+CREATE OR REPLACE TYPE BODY covid_data AS MEMBER FUNCTION porcentaje_infectados(id_pais IN number) RETURN number
+IS resultado number;
+poblacion number;
+BEGIN
+    poblacion:= SELECT SUM(A.poblacion) FROM ESTADO A WHERE A.id_pais = id_pais; 
+    resultado:= SELECT COUNT(*) FROM INFECCIONES A WHERE A.id_pais=id_pais AND A.estado = "infectado";
+    return ROUND(resultado*100/poblacion,2); 
+END;
+--porcentaje_recuperados
+CREATE OR REPLACE TYPE BODY covid_data AS MEMBER FUNCTION porcentaje_recuperados(id_pais IN number) RETURN number
+IS resultado number;
+poblacion number;
+BEGIN
+    total_casos:= SELECT COUNT(*) FROM INFECCIONES A WHERE A.id_pais=id_pais;
+    recuperados:= SELECT COUNT(*) FROM INFECCIONES A WHERE A.id_pais=id_pais AND A.estado = "curado";
+    return ROUND(recuperados*100/total_casos,2); 
+END;
+--porcentaje_fallecidos
+CREATE OR REPLACE TYPE BODY covid_data AS MEMBER FUNCTION porcentaje_recuperados(id_pais IN number) RETURN number
+IS resultado number;
+poblacion number;
+BEGIN
+    total_casos:= SELECT COUNT(*) FROM INFECCIONES A WHERE A.id_pais=id_pais;
+    fallecidos:= SELECT COUNT(*) FROM INFECCIONES A WHERE A.id_pais=id_pais AND A.estado = "muerto";
+    return ROUND(fallecidos*100/total_casos,2); 
+END;
+
+CREATE OR REPLACE TYPE BODY covid_data AS MEMBER FUNCTION porcentaje_infectados RETURN number
+IS resultado number;
+BEGIN
+    IF infectados > poblacion THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Numero de infectados no puede ser mayor a la poblacion');
+    ELSE
+        resultado := ROUND((infectados*100)/poblacion,2);
+        return (resultado);
+    END IF;
+END;
 --Los not null en objetos se verifican con un check en el create table
 CREATE OR REPLACE TYPE persona AS OBJECT (
     img     BLOB,
