a6782f73c4fa3ed732bdf9a96e231216af2066f0
iSlidex <45302075+iSlidex@users.noreply.github.com>
2020-06-19 13:07:23 -0400

    Update TDA.sql

--- TDA.sql
+++ TDA.sql
@@ -63,6 +63,7 @@ BEGIN
 END;
 
 --Los not null en objetos se verifican con un check en el create table
+--size_of_img? falta
 CREATE OR REPLACE TYPE persona AS OBJECT (
     img     BLOB,
     nom1    VARCHAR(15) , --NOT NULL
@@ -75,9 +76,31 @@ CREATE OR REPLACE TYPE persona AS OBJECT (
     STATIC FUNCTION edad (fec_nac DATE) RETURN NUMBER,
     STATIC FUNCTION size_of_img (img BLOB) RETURN NUMBER
 );
+CREATE OR REPLACE TYPE BODY persona AS
+MEMBER FUNCTION edad (fec_nac IN date) return number
+IS res number(4);
+BEGIN
+    IF fec_nac>SYSDATE THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Fecha de nacimiento no puede ser superior a la fecha actual');
+    ELSE 
+        res := MONTHS_BETWEEN(sysdate, fec_nac) / 12;
+        return(res);
+    END IF;
+END;
 /
 CREATE OR REPLACE TYPE historia AS OBJECT (
     fec_i DATE ,  --NOT NULL
     fec_f DATE,
-    STATIC FUNCTION comprobar_fechas (fec_i DATE, fec_f DATE) RETURN NUMBER
+    STATIC FUNCTION comprobar_fechas (fec_i DATE, fec_f DATE) RETURN boolean;
 );
+CREATE OR REPLACE TYPE BODY historia AS
+MEMBER FUNCTION comprobar_fechas (fec_i IN date,fec_f IN date) return boolean
+IS res number(4);
+BEGIN
+    IF fec_i>fec_f THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Fecha de inicio no puede ser superior a la fecha final');
+        return FALSE;
+    ELSE 
+        return TRUE;
+    END IF;
+END;
