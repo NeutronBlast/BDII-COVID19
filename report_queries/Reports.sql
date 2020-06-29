c51bc6edd0514b598d7b3e860794f5a5976d432e
iSlidex <45302075+iSlidex@users.noreply.github.com>
2020-06-29 18:55:32 -0400

    Update Reports.sql

--- report_queries/Reports.sql
+++ report_queries/Reports.sql
@@ -1013,16 +1013,25 @@ END;
 /
 
 -- Reporte 6
-CREATE OR REPLACE PROCEDURE REPORTE_6(ORACLE_REF_CURSOR OUT SYS_REFCURSOR, FECHA_INICIO IN DATE, FECHA_FIN IN DATE,num_pais IN NUMBER) IS
+create or replace PROCEDURE REPORTE_6(ORACLE_REF_CURSOR OUT SYS_REFCURSOR, FECHA_INICIO IN DATE, FECHA_FIN IN DATE,num_pais IN NUMBER) IS
 BEGIN
+    IF num_pais = 0 THEN
+        OPEN ORACLE_REF_CURSOR FOR
+        SELECT 'Mundial' as "nom",null as "bandera",(SELECT COUNT(*) FROM infectados_covid a 
+                WHERE a.estado = 'I' AND (a.hist.fec_f IS NULL) AND (a.hist.fec_i BETWEEN FECHA_INICIO AND i.hist.fec_i)) as "infectados",
+                i.hist.fec_i as "Fecha"
+                FROM infectados_covid i 
+                WHERE i.estado = 'I' AND (i.hist.fec_f IS NULL) AND (i.hist.fec_i BETWEEN FECHA_INICIO AND FECHA_FIN) ORDER BY i.hist.fec_i asc;
+    ELSE
     OPEN ORACLE_REF_CURSOR FOR
         SELECT p.nom,p.bandera,(SELECT COUNT(*) FROM infectados_covid a 
                 JOIN ESTADOS e ON a.id_estado = e.id
                 JOIN PAISES p ON e.id_pais = p.id
-                WHERE p.id = num_pais AND a.estado = 'I' AND (a.hist.fec_i BETWEEN FECHA_INICIO AND i.hist.fec_i)) as "infectados",
+                WHERE p.id = num_pais AND a.estado = 'I' AND (a.hist.fec_f IS NULL) AND (a.hist.fec_i BETWEEN FECHA_INICIO AND i.hist.fec_i)) as "infectados",
                 i.hist.fec_i as "Fecha"
                 FROM infectados_covid i 
                 JOIN ESTADOS e ON i.id_estado = e.id
                 JOIN PAISES p ON e.id_pais = p.id
-                WHERE p.id = num_pais AND i.estado = 'I' AND (i.hist.fec_i BETWEEN FECHA_INICIO AND FECHA_FIN) ORDER BY i.hist.fec_i asc;
+                WHERE p.id = num_pais AND i.estado = 'I' AND (i.hist.fec_f IS NULL) AND (i.hist.fec_i BETWEEN FECHA_INICIO AND FECHA_FIN) ORDER BY i.hist.fec_i asc;
+    END IF;
 END;
